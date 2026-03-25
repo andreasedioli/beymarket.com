ad1d7cd20b3bf8d05dfe97dfd57f8af1530cdc59
farinellafarinella <mrpinkukulele@gmail.com>
2026-03-25 16:11:05 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.BEYMARKET_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAEK5aZM8pTRrSfO9-98SYX-vgOr1MbTHc",
+  authDomain: "beymarket-5535f.firebaseapp.com",
+  projectId: "beymarket-5535f",
+  storageBucket: "beymarket-5535f.firebasestorage.app",
+  messagingSenderId: "829717848432",
+  appId: "1:829717848432:web:00dfd19a7a3c2f05376fb5",
+  measurementId: "G-BY3QW2DXLX"
+};
